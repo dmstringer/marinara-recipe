1742bc242601395ab3989407eca07ab627b7c473
Scott Stringer <scott_ts@hotmail.com>
2020-01-25 13:45:50 -0600

    .

--- script.js
+++ script.js
@@ -2,12 +2,12 @@
 //2) Write out a function definition called marinaraSauce. Be sure to be mindful of your syntax!
 
 function marinaraSauce () {
-  console.log('My Marinara Sauce')
-  console.log('Heat a medium-large saucepan over medium heat')
-  console.log('Add 2 tspns of oil and 5 carlic cloves, cook until golden, about 2 minutes')
-  console.log('Add 1/4 cup water, 2 cans of crushed tomates, and salt and season with black pepper to taste')
-  console.log('Cover the pot, bring to a boil, reduce the heat to medium low, and simmer until the sauce is heated, about 10 minutes')
-  console.log('Stir in 1/4 cup roughly chopped fresh basil, salt and pepper as needed')
+  console.log('My Marinara Sauce');
+  console.log('Heat a medium-large saucepan over medium heat');
+  console.log('Add 2 tspns of oil and 5 carlic cloves, cook until golden, about 2 minutes');
+  console.log('Add 1/4 cup water, 2 cans of crushed tomates, and salt and season with black pepper to taste');
+  console.log('Cover the pot, bring to a boil, reduce the heat to medium low, and simmer until the sauce is heated, about 10 minutes');
+  console.log('Stir in 1/4 cup roughly chopped fresh basil, salt and pepper as needed');
 }
 
-marinaraSauce()
+marinaraSauce();
